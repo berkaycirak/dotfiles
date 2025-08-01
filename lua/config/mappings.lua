4653adff52fe64dd4829288fec4a0c460c980ad8
berkaycirak <berkaycirak_11@hotmail.com>
2025-08-01 17:41:00 +0300

    feat: gitui is added

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -3,3 +3,4 @@ local function map(m, k, v)
 end
 map("n", "<F5>", ":resize +2<CR>")
 map("n", "<A-t>", '<CMD>lua require("FTerm").toggle()<CR>')
+map('t', '<A-t>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
--- lua/plugins/fterm.lua
+++ lua/plugins/fterm.lua
@@ -9,6 +9,22 @@ return {
           width = 0.9,
         },
       })
+      local fterm = require("FTerm")
+      local gitui = fterm:new({
+        ft = 'fterm_gitui',
+        cmd = "gitui",
+        dimensions = {
+          height = 0.9,
+          width = 0.9
+        }
+      })
+      vim.keymap.set('n', '<A-g>', function()
+        gitui:toggle()
+      end)
+
+      vim.keymap.set('t', '<A-g>', function()
+        gitui:toggle()
+      end)
     end
   }
 }
